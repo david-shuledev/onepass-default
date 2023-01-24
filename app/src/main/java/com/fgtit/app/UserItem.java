84dc6926caf24a743fa605808b4e84861728217b
david-shuledev <david.tibanga@shulesoft.africa>
2023-01-24 18:33:58 +0300

    first commit

---
+++ app/src/main/java/com/fgtit/app/UserItem.java
@@ -0,0 +1,20 @@
+package com.fgtit.app;
+
+public class UserItem {
+	
+	public int 	userid;
+	public byte usertype;
+	public byte groupid;
+	public String   username="";
+	public byte[] 	expdate=new byte[3];
+	public byte[]   enlcon1=new byte[5];
+	public byte[]   enlcon2=new byte[5];
+	public byte[]   enlcon3=new byte[5];
+	public byte[]	fp1=new byte[512];
+	public byte[]	fp2=new byte[512];
+	public byte[]	fp3=new byte[512];
+	public byte[]   enllNO=new byte[4];
+	public String	photo="";
+	public String	phone="";
+	public int 	gender=0;
+}
---
+++ app/src/main/java/com/fgtit/utils/ToastUtil.java
@@ -0,0 +1,35 @@
+package com.fgtit.utils;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.widget.Toast;
+
+public class ToastUtil {
+
+	public static void showToast(Context context,String msg) {
+         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+	}
+	
+	
+	public static void showToast(Context context,int resId) {
+        Toast.makeText(context, resId, Toast.LENGTH_SHORT).show();
+	}
+
+	public static void showToastTop(Context context,String msg) {
+		Toast toast=Toast.makeText(context, msg, Toast.LENGTH_SHORT/*2000 */);
+		toast.setGravity(Gravity.TOP|Gravity.CENTER, 0, 120);  
+		toast.show();
+	}
+
+	public static void showToastDef(Context context,String msg,int pos,int tm) {
+		Toast toast=Toast.makeText(context, msg, tm);
+		toast.setGravity(Gravity.TOP|Gravity.CENTER, 0, pos);  
+		toast.show();
+	}
+	
+	public static void showToastEx(Context context,String msg,int tm) {
+		Toast toast=Toast.makeText(context, msg, tm);
+		toast.setGravity(Gravity.CENTER, 0, 0);  
+		toast.show();
+	}
+}
